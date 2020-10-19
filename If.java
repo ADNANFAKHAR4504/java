76e066fd3fdb35c5a22fd76f1564c97c27765f67
unknown <adnanfakhar6@gmail.com>
2020-10-19 09:03:58 -0700

    if else

---
+++ If.java
@@ -0,0 +1,13 @@
+public class If{
+	public static void main(String[] args) {
+	int i=0;
+	if(i==1)   
+	   {
+		System.out.println("YES");
+	}
+else
+{
+	System.out.println("no");	
+}
+        }
+    }
---
+++ Printnumber.java
@@ -0,0 +1,12 @@
+public class Printnumber{
+ public static void main(String[] args) {
+ 	int i=1;
+ 	//rercusive function is used here:
+ 	  static void Printnumber(i);
+ 	if(i<=5){
+        System.out.println(i);
+ 		i++;
+ 		system.out.println Printnumber(i); //calling rcursiv function
+ 	}
+ }
+}
